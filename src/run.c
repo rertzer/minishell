80f5e032a0224ecad718b96b4287137b2662f717
rertzer <rertzer@student.42.fr>
2023-03-20 10:47:22 +0100

    malloc protected

--- src/run.c
+++ src/run.c
@@ -6,7 +6,7 @@
 /*   By: rertzer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 16:15:03 by rertzer           #+#    #+#             */
-/*   Updated: 2023/03/19 13:04:36 by rertzer          ###   ########.fr       */
+/*   Updated: 2023/03/20 10:34:28 by rertzer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,12 @@ static int	pp_run_make_pipes(t_msdata *msdata)
 	msdata->pipefd = malloc(sizeof(int) * 2 * (msdata->cmd_nb - 1));
 	if (NULL == msdata->pipefd)
 		return (ms_return_error(1, R_MAL));
+	i = -1;
+	while (++i < msdata->cmd_nb - 1)
+	{
+		msdata->pipefd[i][0] = -1;
+		msdata->pipefd[i][1] = -1;
+	}
 	i = -1;
 	while (++i < (msdata->cmd_nb - 1))
 	{
@@ -77,11 +83,15 @@ void	pp_run_close_pipes(t_msdata *msdata)
 {
 	int	i;
 
+	if (msdata->pipefd == NULL)
+		return ;
 	i = -1;
 	while (++i < (msdata->cmd_nb - 1))
 	{
-		close(msdata->pipefd[i][0]);
-		close(msdata->pipefd[i][1]);
+		if (msdata->pipefd[i][0] != -1)
+			close(msdata->pipefd[i][0]);
+		if (msdata->pipefd[i][1] != -1)
+			close(msdata->pipefd[i][1]);
 	}
 }
 
